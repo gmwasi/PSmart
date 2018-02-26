71567910b04becd94e5415228164be7344c60d93
GMwasi <mwasi.brian@thepalladiumgroup.com>
2018-02-26 21:39:17 +0300

    App with intent

---
+++ app/src/main/java/org/kenyahmis/psmart/CardReaderActivity.java
@@ -0,0 +1,46 @@
+package org.kenyahmis.psmart;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+public class CardReaderActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_reader);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        String text = getIntent().getExtras().getString("WRITE_DATA");
+        if (text != null) {
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+            readJob();
+        }
+    }
+
+    private void readJob()  {
+        finish();
+    }
+
+    @Override
+    public void finish() {
+        setResult(0);
+        super.finish();
+    }
+}
